afd631efa4c0c99b88544c5082e3ebc82475cadf
Melissa Khat <melissa.khat@gmail.com>
2015-02-22 12:47:38 -0700

    display error message

--- crud-from-scratch-with-validations/app/models/status.rb
+++ crud-from-scratch-with-validations/app/models/status.rb
@@ -2,4 +2,9 @@ class Status < ActiveRecord::Base
   has_many :likes
 
   validates :status, presence: true
+
+  status = Status.new(status: "A new status")
+  status.valid? # => true
+  status.errors 
+    # => ["Status can't be blank."]
 end
